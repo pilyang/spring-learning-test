5aa00f789441e389dee03c2c99797bf7fab2b468
pilyang <30036534+pilyang@users.noreply.github.com>
2023-04-12 22:02:29 +0900

    solve: MethodArgumentTest

--- src/main/java/nextstep/helloworld/mvc/handler/MethodArgumentController.java
+++ src/main/java/nextstep/helloworld/mvc/handler/MethodArgumentController.java
@@ -12,8 +12,21 @@ import java.util.List;
 @RequestMapping("/method-argument")
 public class MethodArgumentController {
 
+    /**
+     * @ModelAttribute 도 동일하게 사용가능해보이기는 함
+     * @ModelAttribute
+     * - setter를 통해서 mapping을 하고, 그 과정에서 type에 대한 vlidation이 진행된다함
+     * - parameter가 여러개일경우 하나의 객체에 한번에 맵핑 가능
+     * @RequestParam
+     * - 파라미터와 1:1 맵핑
+     *
+     * ex)
+     * 요청이 "/method-argument/users?name=test&email=testmail"과 같은 경우
+     * requestParam(@ModelAttribute User user)와 같이 받기 가능 (단 User에 getter가 있어야함)
+     */
+
     @GetMapping("/users")
-    public ResponseEntity<List<User>> requestParam(String userName) {
+    public ResponseEntity<List<User>> requestParam(@RequestParam("name") String userName) {
         List<User> users = Arrays.asList(
                 new User(userName, "email"),
                 new User(userName, "email")
@@ -22,7 +35,7 @@ public class MethodArgumentController {
     }
 
     @PostMapping("/users/body")
-    public ResponseEntity requestBody(User user) {
+    public ResponseEntity requestBody(@RequestBody User user) {
         User newUser = new User(1L, user.getName(), user.getEmail());
         return ResponseEntity.created(URI.create("/users/" + newUser.getId())).body(newUser);
     }
