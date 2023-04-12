b1c44c6a5f6cd3e90fa55d806332f794d2c8c49b
pilyang <30036534+pilyang@users.noreply.github.com>
2023-04-12 22:20:59 +0900

    solve: ReturnValueTest

--- src/main/java/nextstep/helloworld/mvc/handler/ReturnValueController.java
+++ src/main/java/nextstep/helloworld/mvc/handler/ReturnValueController.java
@@ -10,23 +10,41 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/return-value")
 public class ReturnValueController {
 
-//    public void string() {
-//        return "message";
-//    }
-//
-//    public void responseBodyForUser() {
-//        return new User("name", "email");
-//    }
-//
-//    public void responseEntity(@PathVariable Long id) {
-//        return ResponseEntity.ok(new User("name", "email"));
-//    }
-//
-//    public void responseEntityFor400() {
-//        return ResponseEntity.badRequest().build();
-//    }
-//
-//    public void thymeleaf() {
-//        return "sample";
-//    }
+
+    /**
+     * @ResponseBody : Return 값이 response body로 포장되도록 함..
+     */
+    @GetMapping("/message")
+    @ResponseBody
+    public String string() {
+        return "message";
+    }
+
+
+    @GetMapping("/users")
+    @ResponseBody
+    public User responseBodyForUser() {
+        return new User("name", "email");
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity responseEntity(@PathVariable Long id) {
+        return ResponseEntity.ok(new User("name", "email"));
+    }
+
+    @GetMapping("/members")
+    public ResponseEntity responseEntityFor400() {
+        return ResponseEntity.badRequest().build();
+    }
+
+    /**
+     * Model, thymeleaf 작동 관해서 찾아보기
+     * @param model
+     * @return resources/templates/sample.html
+     */
+    @GetMapping("thymeleaf")
+    public String thymeleaf(Model model) {
+        model.addAttribute("name", "필립");
+        return "sample";
+    }
 }
